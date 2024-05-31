0953baacc052fc5e76620eda5eac9f98debb5394
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-05-31 15:27:38 +0700

    masukkan case 4

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -109,6 +109,10 @@ int main () {
                 q.display();
                 break;
                 }
+                case '4' : {
+                    return 0;
+                
+                }
         }
     }
 }
